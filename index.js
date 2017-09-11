5a1c22d3dd3ff2398f646c185bcce269d430e765
M V <vahyno+learn@gmail.com>
2017-09-11 19:04:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ var funkyFunction = function() {
 // NOTE: To pass this final test, you only need to modify the code below this line.
 var theFunk = funkyFunction()();
 
-theFunk
+console.log(theFunk)
